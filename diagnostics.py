2ec89fd78b81d3ec463547b8b5941de978cf3d90
bifrost <dra@debitoor.com>
2023-04-05 15:13:06 +0200

    add diagnostics and reporting

--- diagnostics.py
+++ diagnostics.py
@@ -4,37 +4,83 @@ import numpy as np
 import timeit
 import os
 import json
+import pickle
+import subprocess
+from io import StringIO
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
 
 ##################Function to get model predictions
 def model_predictions():
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    with open(os.path.join(prod_deployment_path, 'trainedmodel.pkl'), 'rb') as file:
+        model = pickle.load(file)
+
+    df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+
+    x = df.iloc[:,1:-1].values.reshape(-1, 3)
+
+    y_pred = model.predict(x)
+
+    return y_pred #return value should be a list containing all predictions
 
 ##################Function to get summary statistics
 def dataframe_summary():
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    x = df.iloc[:,1:-1]
+
+    result = np.array([x.mean(), x.median(), x.std()]).flatten().tolist()
+    return result #return value should be a list containing all summary statistics
+
+def missing_data():
+    #calculate what percent of each column consists of NA values
+    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    x = df.iloc[:,1:-1]
+
+    nas = list(x.isna().sum())
+    result = [nas[i]/len(x.index) for i in range(len(nas))]
+    return result
 
 ##################Function to get timings
+def ingestion_timing():
+    starttime = timeit.default_timer()
+    os.system('python3 ingestion.py')
+    timing = timeit.default_timer() - starttime
+    return timing
+
+def training_timing():
+    starttime = timeit.default_timer()
+    os.system('python3 training.py')
+    timing = timeit.default_timer() - starttime
+    return timing
+
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+
+    result = [ingestion_timing(), training_timing()]
+    return result #return a list of 2 timing values in seconds
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    #get a list of
+
+    outdated = subprocess.check_output(['pip', 'list','--outdated']).decode("utf-8")
+    df = pd.read_csv(StringIO(outdated), skiprows=1, sep=' +', engine='python')
+    result = df.iloc[:,:-1].values
+    return result
 
 
 if __name__ == '__main__':
     model_predictions()
     dataframe_summary()
+    missing_data()
     execution_time()
     outdated_packages_list()
 
@@ -42,4 +88,4 @@ if __name__ == '__main__':
 
 
 
-    
+
--- reporting.py
+++ reporting.py
@@ -7,25 +7,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 ###############Load config.json and get path variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-
-
-
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+model_path = os.path.join(config['output_model_path'])
 
 ##############Function for reporting
 def score_model():
     #calculate a confusion matrix using the test data and the deployed model
     #write the confusion matrix to the workspace
+    with open(os.path.join(prod_deployment_path, 'trainedmodel.pkl'), 'rb') as file:
+        model = pickle.load(file)
 
+    df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
 
+    x = df.iloc[:,1:-1].values.reshape(-1, 3)
+    y = df.iloc[:,-1:].values.reshape(-1, 1).ravel()
 
+    # Note: depricated
+    #cm = metrics.plot_confusion_matrix(model, x, y)
+    #plt.savefig(os.path.join(model_path, 'confusionmatrix.png'))
+
+    y_pred = model.predict(x)
+    cm =  confusion_matrix(y, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+    fig = disp.plot()
+    plt.savefig(os.path.join(model_path, 'confusionmatrix.png'), dpi=300)
 
 
 if __name__ == '__main__':
